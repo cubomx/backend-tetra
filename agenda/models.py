27667e8abbe1aefeca387897c7a26dd93bfa3292
cubomx <cheservelram657@gmail.com>
2024-04-15 17:38:35 -0600

    ADD: New function - Validation of JSON keys

--- agenda/models.py
+++ agenda/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Event(models.Model):
-    nombre = models.CharField(max_length=50)
-    fecha = models.DateTimeField("Fecha de evento")
--- agenda/views.py
+++ agenda/views.py
@@ -16,10 +16,23 @@ def index(request):
     data = list(event_details)
     
     json_data = json_util.dumps(data)
+    
 
     print(json_data)
     response = HttpResponse(json_data, content_type='application/json')
     return response
+
+def json_contains_key(data, keys):
+    for key in keys:
+        if key not in data:
+            return (False, "Missing key {} in data sent".format(key))
+        #elif type(data[key]) is not types[]
+        
+    return (True, "Data seems good")
+
+def checkEventoData(data):
+    keys = ['name', 'clients', 'type', 'date', 'location', 'num_of_people', 'cost', 'upfront']
+    return json_contains_key(data, keys)
 
 
 def addEvento(request):
@@ -27,13 +40,13 @@ def addEvento(request):
     print(data)
     print(data['name'])
 
+    print(checkEventoData(data))
+
+    print(type(data['name']))
+
     json_data = json_util.dumps([{"foo": "bar"}])
     response = HttpResponse(json_data, content_type='application/json')
     return response
-
-#client = pymongo.MongoClient('mongodb://root:example@localhost/tetra')
-
-
 
 
 '''event = {
